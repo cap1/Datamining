1fc77d5075e5547505e1bb6d91274d04b8e4079f
Christian Mueller <chrismul@bi-89.gobics.de>
2011-07-25 12:21:04 +0200

    maximum variance - not plotted yet

--- blatt1/aufgabe2.m
+++ blatt1/aufgabe2.m
@@ -9,4 +9,21 @@ XdataMat = rand(nDims,nVecs);
 
 meanVec = mean(XdataMat,nDims);
 XshiftDataMat = XdataMat-repmat(meanVec,1,nVecs);
-plot(XshiftDataMat(1,:),XshiftDataMat(2,:),'x')
+%plot(XshiftDataMat(1,:),XshiftDataMat(2,:),'xr');
+
+maxAlphaAngle =0;
+maxVar = 0;
+for alphaAngle=0:360
+
+	RotMat = [ cos(alphaAngle), sin(alphaAngle);-sin(alphaAngle), cos(alphaAngle) ];
+	XrotShiftDataMat = RotMat*XshiftDataMat;
+	xVar = var(XrotShiftDataMat(:,1));
+	if xVar > maxVar
+		maxVar = xVar;
+		maxAlphaAngle = alphaAngle;
+	end
+end
+
+maxVar
+maxAlphaAngle
+%plot(XrotShiftDataMat(1,:),XrotShiftDataMat(2,:),'xb');
