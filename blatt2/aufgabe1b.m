80623fc1dc20f254bf2dcddb1184ee1229e7fac6
Christian Mueller <chrismul@bi-89.gobics.de>
2011-07-26 11:53:38 +0200

    seems to work

--- blatt2/aufgabe1b.m
+++ blatt2/aufgabe1b.m
@@ -9,27 +9,21 @@ DataSetMaxVarMat =[];
 for i=1:nDataSets
 	XdataMat = rand(nDims,nVecs);
 
-
+	XdataMat = TransMat*rand(nDims,nVecs);
 	meanVec = mean(XdataMat,nDims);
 	XdataMat = XdataMat-repmat(meanVec,1,nVecs);
-	XdataMat = TransMat*rand(nDims,nVecs);
-
-%	plot(XdataMat(1,:),XdataMat(2,:),'x')
 
 	mVar = 100;
 	vVar = rand(nDims,1);
 	[eigenVec] = firstEigenVec (mVar, vVar, XdataMat)
 	%Eigenvec liegt von nullpunkt aus in richtung der ersten hauptkomponente
 	%	== richtung groesster varianz
-
-	CovMat = cov(XdataMat');
-	vCvMat =[];
-	for alphaAngle=1:360
-		DirVec = [cos(alphaAngle), sin(alphaAngle) ]';
-		vCv = DirVec' * CovMat * DirVec;
-		vCvMat = [ vCvMat ; alphaAngle, vCv ]; 
-	end
-	[maxvCv, maxvCvAlphaAngle] = max(vCvMat(:,2))
-%	plot(iTerMat(1,:),iTerMat(2,:),'lr');
+	[Val, Vec ] = eig(cov(XdataMat'))
+	hold on
+	plot(XdataMat(1,:),XdataMat(2,:),'*')
+	plot(eigenVec(1),eigenVec(2), "xr")
+	plot(0,0, "og")
+	plot(Val(:,1),Val(:,2), "xg")
+	hold off
 end
 
--- blatt2/firstEigenVec.m
+++ blatt2/firstEigenVec.m
@@ -1,6 +1,7 @@
 function [WVec] = firstEigenVec (mVar, vVar, XdataMat)
 
-	CovMat = cov(XdataMat');
+	CovMat = cov(XdataMat')
+	vVar
 	for iTer=1:mVar
 		WVec = CovMat*vVar;
 		vVar = WVec/norm(WVec);
