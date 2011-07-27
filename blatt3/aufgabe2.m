c701a05fc546dfb067d912927887998e9399959a
Christian Mueller <chrismul@bi-89.gobics.de>
2011-07-27 14:29:21 +0200

    no memory remains

--- blatt3/aufgabe2.m
+++ blatt3/aufgabe2.m
@@ -3,11 +3,11 @@
 
 path(path,'../funktions/');
 
-load("../data/EcoliRSCU.mat")
+%load("../data/EcoliRSCU.mat")
 %load("../data/BsubRSCU.mat")
 % Variables are 'Vectors' and 'Labels'
 
-[nDims, nVecs ] = size(Vectors);
+%[nDims, nVecs ] = size(Vectors);
 
 %DistMat = zeros(nVecs);
 
@@ -20,3 +20,21 @@ load("../data/EcoliRSCU.mat")
 %save("-ascii", "../data/Dist.ascii",DistMat);
 
 load('/c1/scratch/ralph/distmat.mat');
+nVecs = size(DistMat,2);
+
+SquareDistMat = DistMat.^2;
+
+RowMeanVec = mean(SquareDistMat,2);
+ColMeanVec = mean(SquareDistMat,1);
+MeanVal = 1/(nVecs^2) * (sum(sum(SquareDistMat,1),2));
+
+GramMat = -1/2 * ( SquareDistMat - repmat(RowMeanVec,size(SquareDistMat)) - repmat(ColMeanVec,size(SquareDistMat)) + repmat(MeanVal,size(SquareDistMat)));
+
+nHK = 3;
+mVar = 300;
+vVar = rand(nDim,1);
+
+EigMat = nEigenVec(nHK,mVar,vVar,GramMat);
+ProjMat = EigMat'*GramMat;
+
+plot(ProjMat(1,:),ProjMat(2,:),"or");
